85645ae42682ce548a835d730b5897a3082ab371
Carlos Blázquez Sumastre <cbsumastre@gmail.com>
2025-11-14 08:57:33 +0100

    added DetalleVentaDTO and VentaDTO

--- src/main/java/com/firstflip/supermarket/dto/DetalleVentaDTO.java
+++ src/main/java/com/firstflip/supermarket/dto/DetalleVentaDTO.java
@@ -1,5 +1,20 @@
 package com.firstflip.supermarket.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DetalleVentaDTO {
+
+  private Long id;
+  private String nombreProducto;
+  private Integer cantidadProducto;
+  private Double precio;
+  private Double subtotal;
 
 }
--- src/main/java/com/firstflip/supermarket/dto/VentaDTO.java
+++ src/main/java/com/firstflip/supermarket/dto/VentaDTO.java
@@ -1,5 +1,28 @@
 package com.firstflip.supermarket.dto;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class VentaDTO {
+
+  // datos de la venta
+  private Long id;
+  private LocalDateTime fecha;
+  private String estado;
+  private Double total;
+
+  // datos de la sucursal
+  private Long idSucursal;
+
+  // detalles de la venta
+  private List<DetalleVentaDTO> detalle;
 
 }
