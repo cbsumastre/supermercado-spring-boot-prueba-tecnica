ca79b8003e862ad6a72dbf60c18681bafa6af173
Carlos Blázquez Sumastre <cbsumastre@gmail.com>
2025-11-15 12:54:51 +0100

    added categoria producto description

--- src/main/java/com/firstflip/supermarket/model/CategoriaProducto.java
+++ src/main/java/com/firstflip/supermarket/model/CategoriaProducto.java
@@ -1,23 +1,43 @@
 package com.firstflip.supermarket.model;
 
 public enum CategoriaProducto {
-  ELECTRONICA_TECNOLOGIA,
 
-  HOGAR_COCINA,
+  ELECTRONICA_TECNOLOGIA("Dispositivos digitales, componentes de PC, redes y accesorios móviles."),
 
-  MUEBLES_OFICINA,
+  HOGAR_COCINA(
+      "Electrodomésticos, utensilios de cocina, artículos de limpieza y sistemas de seguridad inteligentes para el hogar."),
 
-  AUDIO_VIDEO,
+  MUEBLES_OFICINA(
+      "Mobiliario para trabajo y descanso, soluciones de almacenamiento y decoración exterior."),
 
-  FOTOGRAFIA_DRONES,
+  AUDIO_VIDEO(
+      "Televisores, equipos de sonido, instrumentos musicales y todo lo relacionado con la producción y consumo multimedia."),
 
-  DEPORTES_OCIO,
+  FOTOGRAFIA_DRONES(
+      "Cámaras, lentes, accesorios fotográficos y vehículos aéreos no tripulados (drones)."),
 
-  LIBROS_ARTE,
+  DEPORTES_OCIO(
+      "Equipamiento para fitness, material deportivo, camping y artículos para actividades al aire libre."),
 
-  JARDINERIA_BRICOLAJE,
+  LIBROS_ARTE("Material de lectura, pintura, dibujo y otros suministros para hobbies creativos."),
 
-  JUGUETES_JUEGOS,
+  JARDINERIA_BRICOLAJE(
+      "Herramientas, maquinaria, insumos para el jardín y materiales de construcción o reparación doméstica."),
 
-  MODA_CUIDADO_PERSONAL
+  JUGUETES_JUEGOS(
+      "Artículos para entretenimiento infantil y adulto, incluyendo consolas, juegos de mesa y construcción."),
+
+  MODA_CUIDADO_PERSONAL(
+      "Ropa, calzado, accesorios de viaje y productos de belleza, higiene y afeitado.");
+
+  private final String descripcion;
+
+  public String getDescripcion() {
+    return descripcion;
+  }
+
+
+  private CategoriaProducto(String description) {
+    this.descripcion = description;
+  }
 }
